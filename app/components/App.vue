d5630740222943c25d493661991dfd9a4e052932
P-a <57420066+Barbouy@users.noreply.github.com>
2020-09-11 17:21:25 +0200

    first commit

---
+++ app/components/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <Page>
+        <GridLayout columns="*" rows="*" iosOverflowSafeArea="false">
+            <WebView col="0" row="0" src="http://192.168.1.75:8080/"/>
+        </GridLayout>
+    </Page>
+</template>
+
+<script >
+import { messaging } from "nativescript-plugin-firebase/messaging";
+
+export default {
+ 
+mounted() {
+  messaging.registerForPushNotifications({
+  onPushTokenReceivedCallback: (token) => {
+    console.log("Firebase plugin received a push token: " + token);
+  },
+
+  onMessageReceivedCallback: (message) => {
+    console.log("Push message received: " + message.title);
+  }
+}).then(() => console.log("Registered for push"));
+},
+
+}
+
+  
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #233155;
+    }
+
+    Page {
+      background-color: #233155;
+    }
+</style>
